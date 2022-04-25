e123f658f241341d3cdc2948db1f45d77bb2e52b
connorhsm <connorhsmith@gmail.com>
2022-04-25 14:19:43 +1000

    Reorder update script

--- utils/update.sh
+++ utils/update.sh
@@ -2,13 +2,13 @@
 set -e
 
 cd /var/www/twotech
-if ./utils/changes_upstream.sh "./process/OneLifeData7"; then
-    node process download
-fi
-
 if ./utils/changes_upstream.sh; then
     npm install
     npm run build
     cd process
     npm install
+fi
+
+if ./utils/changes_upstream.sh "./process/OneLifeData7"; then
+    node process download
 fi
