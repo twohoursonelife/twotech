215efbf7e5b1fef6530c22ce28b7bbbe1033b2b4
connorhsm <connorhsmith@gmail.com>
2022-05-05 00:07:29 +1000

    Added more log info and comments to update script

--- utils/update.sh
+++ utils/update.sh
@@ -3,15 +3,22 @@ set -e
 
 cd /var/www/twotech
 if ./utils/changes_upstream.sh; then
+    echo "Upstream changes made to twotech, pulling and updating..."
     git pull
     npm install
     npm run build
+    # Update packages for the process module also
     cd process
     npm install
     cd ..
+else
+    echo "No upstream changes to twotech, moving on..."
 fi
 
 if ./utils/changes_upstream.sh "./process/OneLifeData7"; then
+    echo "Upstream changes made to game data, running data update..."
     # git pull is not necessary here as the following command will handle that.
     node process download
+else
+    echo "No upstream changes to data, update check complete."
 fi
