d5d323fa2ad83bfdacf59c17e5d470e4d398471d
Connor Smith <connorhsmith@gmail.com>
2023-11-07 20:07:07 +1100

    Disable version exporting
    Long standing issue regarding commit history, better off losing this functionality than spending any more time fixing it

--- process/src/MainProcessor.js
+++ process/src/MainProcessor.js
@@ -68,8 +68,8 @@ class MainProcessor {
     console.log("Exporting objects...");
     gameData.exportObjects();
 
-    console.log("Exporting versions...");
-    gameData.exportVersions();
+    // console.log("Exporting versions...");
+    // gameData.exportVersions();
 
     console.log("Exporting biomes...");
     gameData.exportBiomes();
