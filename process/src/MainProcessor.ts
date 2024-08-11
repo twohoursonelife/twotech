1df7769143fb0e59d7f12d272891ec3895193f0e
Matt Klass <matt.klass@telosalliance.com>
2024-08-11 17:16:40 +0000

    MainProcessor: Add code and printouts for timing each processing function call

--- process/src/MainProcessor.ts
+++ process/src/MainProcessor.ts
@@ -30,64 +30,107 @@ class MainProcessor {
   process(version: ChangeLogVersion): ChangeLogVersion {
     // double-equals used to cover undefined case too.
     const gameData = new GameData(this.processDir, this.dataDir(), this.staticDir(version == null));
+    console.time("Processing took");
 
     if (this.doDownload) {
-      console.log("Downloading data...");
+      console.log("\nDownloading data...");
+      console.time("Downloading data took");
       gameData.download(this.gitUrl());
+      console.timeEnd("Downloading data took");
     } else {
       gameData.verifyDownloaded();
     }
 
     if (version) {
       console.log(`Checking out v${version.id}...`);
+      console.timeEnd(`Checking out v${version.id} took`);
       gameData.checkoutVersion(version);
+      console.timeEnd(`Checking out v${version.id} took`);
     } else {
       gameData.checkoutMaster();
     }
 
-    console.log("Preparing directories...");
+    console.log("\nPreparing directories...");
+    console.time("Preparing directories took");
     gameData.prepareStaticDir();
+    console.timeEnd("Preparing directories took");
 
-    console.log("Importing objects...");
+    console.log("\nImporting objects...");
+    console.time("Importing objects took");
     gameData.importObjects();
+    console.timeEnd("Importing objects took");
+
+    console.log("\nImporting categories...");
+    console.time("Importing categories took");
     gameData.importCategories();
+    console.timeEnd("Importing categories took");
+
+    console.log("\nImporting transitions...");
+    console.time("Importing transitions took");
     gameData.importTransitions();
+    console.timeEnd("Importing transitions took");
+
+    console.log("\nImporting biomes...");
+    console.time("Importing biomes took");
     gameData.importBiomes();
+    console.timeEnd("Importing biomes took");
 
-    console.log("Populating versions...");
+    console.log("\nPopulating versions...");
+    console.time("Populating versions took");
     gameData.populateVersions();
+    console.timeEnd("Populating versions took");
 
-    console.log("Calculating object depth...");
+    console.log("\nCalculating object depth...");
+    console.time("Calculating object depth took");
     gameData.calculateObjectDepth();
+    console.timeEnd("Calculating object depth took");
 
     if (this.doSprites) {
-      console.log("Converting sprite images...");
+      console.log("\nConverting sprite images...");
+      console.time("Converting sprite images took");
       gameData.convertSpriteImages();
-      gameData.convertGroundImages();
+      console.timeEnd("Converting sprite images took");
 
-      console.log("Processing sprites...");
+      console.log("\nConverting ground images...");
+      console.time("Converting ground images took");
+      gameData.convertGroundImages();
+      console.timeEnd("Converting ground images took");
+
+      console.log("\nProcessing sprites...");
+      console.time("Processing sprites took");
       gameData.processSprites();
+      console.timeEnd("Processing sprites took");
     }
 
     if (this.doSounds) {
-      console.log("Converting sound files...");
+      console.log("\nConverting sound files...");
+      console.time("Converting sound files took");
       gameData.convertSounds();
+      console.timeEnd("Converting sound files took");
     }
 
-    console.log("Exporting objects...");
+    console.log("\nExporting objects...");
+    console.time("Exporting objects took");
     gameData.exportObjects();
+    console.timeEnd("Exporting objects took");
 
-    // console.log("Exporting versions...");
+    // console.log("\nExporting versions...");
     // gameData.exportVersions();
 
-    console.log("Exporting biomes...");
+    console.log("\nExporting biomes...");
+    console.time("Exporting biomes took");
     gameData.exportBiomes();
+    console.timeEnd("Exporting biomes took");
 
     if (version) {
-      console.log("Generating sitemap...");
+      console.log("\nGenerating sitemap...");
+      console.time("Generating sitemap took");
       gameData.generateSitemap();
+      console.timeEnd("Generating sitemap took");
       return null;
     }
+
+    console.timeEnd("Processing took");
 
     if (process.env.ONETECH_MOD_NAME) {
       return null;
